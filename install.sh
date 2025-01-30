aa592596fd093db69f33d2e27b9531abb7d1caf1
uzzipanel-dev <118691276+uzzipanel-dev@users.noreply.github.com>
2025-01-30 22:59:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ set -e
 
 export GITHUB_SOURCE="main"
 export SCRIPT_RELEASE="canary"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/uzzipanel-dev/panel-installer/main/install.sh"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/uzzipanel-dev/panel-installer/"
 
 LOG_PATH="/var/log/uzzi-installer.log"
 
